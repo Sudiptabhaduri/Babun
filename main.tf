990b8681826bb657fc7b2606e736ad9d226184c9
subhadur <subhadur@cisco.com>
2024-11-12 10:14:38 +0530

    updated code

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "rg-sudip" {
   name     = sudip_rg
   location = centralindia
 }
+
+resource "azurerm_resource_group" "rg-babun" {
+  name     = babun_rg
+  location = centralindia
+}
